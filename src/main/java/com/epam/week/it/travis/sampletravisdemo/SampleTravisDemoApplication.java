ffdf0af63e1a05aa059165f81432ede25fe48735
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2018-02-20 17:04:14 +0300

    Basic Spring Boot project

---
+++ src/main/java/com/epam/week/it/travis/sampletravisdemo/SampleTravisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.epam.week.it.travis.sampletravisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTravisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTravisDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epam/week/it/travis/sampletravisdemo/SampleTravisDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.week.it.travis.sampletravisdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleTravisDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
